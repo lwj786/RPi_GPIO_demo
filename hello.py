c23bf99a27ff4c71cdd78aa4beeefe43be010ac2
lwj786 <lwj7863439@gmail.com>
2017-06-17 21:05:53 +0800

    update hello.py 17.06.17

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import stdio_manager as siom
+
 def hello(string):
     if len(string) == 0:
         string = "World"
@@ -10,7 +12,28 @@ def main(string_list):
     hello(" ".join(string_list))
 
 def behavior(gpio_list, params_list):
-    main(params_list)
+    global print
+
+    name = "hello"
+    content_list = []
+    for parameter in params_list:
+        if parameter[:7] == "--name-":
+            name = parameter[7:]
+        else:
+            content_list.append(parameter)
+
+    def output_with_identity(index):
+        def push_output(content):
+            siom.push_output(content, index)
+
+        return push_output
+
+    index = siom.register(name)
+    print = output_with_identity(index)
+
+    main(content_list)
+
+    siom.unregister(index)
 
 if __name__ == "__main__":
     from sys import argv
