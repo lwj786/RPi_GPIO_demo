f5690cff3ad25ae10f6fba6498c5fe22745540fa
lwj786 <lwj7863439@gmail.com>
2017-07-31 23:41:13 +0800

    clean up code: call_me.py & hello.py 17.07.31

--- call_me.py
+++ call_me.py
@@ -4,10 +4,12 @@ import stdio_manager as siom
 
 NEED_GPIO = False
 
-def trigger(gpio_list, _name, params_list):
+''' func: trigger()
+    interface for demo.py, trigger condition: standard input '''
+def trigger(channel_list, _name, params):
     name = "me"
 
-    for parameter in params_list:
+    for parameter in params:
         if parameter[:7] == "--name-":
             name = parameter[7:]
 
